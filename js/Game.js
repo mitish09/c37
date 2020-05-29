ec2b5969c0ff55157aeab5539b7d62491aaa9210
mitish09 <sundevelg@gmail.com>
2020-05-29 15:59:06 +0530

    https://github.com/mitish09/c37.git

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1",car1Image);
     car2 = createSprite(300,200);
+    car2.addImage("car2",car2Image);
     car3 = createSprite(500,200);
+    car3.addImage("car3",car3Image);
     car4 = createSprite(700,200);
+    car4.addImage("car4",car4Image);
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +46,15 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background("grey");
+      image(track,0,-displayHeight*4,displayWidth,displayHeight*5)
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -74,11 +80,17 @@ class Game {
 
     }
 
-    if(keyIsDown(UP_ARROW) && player.index !== null){
+    if(keyIsDown(UP_ARROW)&& player.index !== null){
       player.distance +=10
       player.update();
     }
-
+    if(player.distance>3860){
+      gameState = 2
+    }
     drawSprites();
+  }
+  end(){
+    game.update(2);
+    console.log("game ended");
   }
 }
